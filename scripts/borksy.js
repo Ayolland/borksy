e60e7ac9c95d7df92421a4e1790e9ce0e5f5bec1
Andrew Yolland <and.t.yoll@gmail.com>
2018-04-12 13:38:10 -0500

    accordions added, styling stuff

--- scripts/borksy.js
+++ scripts/borksy.js
@@ -6,6 +6,62 @@ function setSaveTrigger($this){
 	});
 }
 
+function loadTemplate(){
+	var $ajax = $.ajax('template/template.html');
+	$ajax.done(function(){
+		loadedFiles['template.html'] = $ajax.responseText;
+	});
+}
+
+function assembleAndDownloadFile(){
+	$('[data-save]').each(function(){
+		saveThisData($(this));
+	});
+
+	var modifiedTemplate = loadedFiles['template.html'].repeat(1);
+	$('[data-borksy-replace-single]').each(function(){
+		var $this = $(this);
+		var valueToReplace = 'BORKSY-' + $this.data('borksy-replace-single');
+		var formValue = $this.val();
+		modifiedTemplate = modifiedTemplate.replace(valueToReplace, formValue);
+	});
+	$('[data-borksy-replace-single]').promise().done(function(){
+		download('myBORKSYgame.html', modifiedTemplate);
+	});
+}
+
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+    console.log("File '" + filename + "' downloaded")
+}
+
+function saveThisData($this){
+	var name = $this.attr('name');
+	var value = $this.val();
+	localStorage.setItem(name, value);
+	console.log("Key: '" + name + "' saved to localStorage");
+}
+
+function loadThisData($this){
+	var name = $this.attr('name');
+	var value = localStorage.getItem(name);
+	$this.val(value);
+	console.log(" Got key: " + name + " from localStorage");
+}
+
+function togglePartyMode(){
+	$('body').toggleClass('party');
+}
+
 function loadDefaults(checkSaveData = true){
 	$('[data-save]').each(function(){
 		var $thisField = $(this);
@@ -66,63 +122,26 @@ function restoreDefaults(){
 	}
 }
 
-function loadTemplate(){
-	var $ajax = $.ajax('template/template.html');
-	$ajax.done(function(){
-		loadedFiles['template.html'] = $ajax.responseText;
+function activateCollapsibles(){
+	$('[data-collapsible]').each( function(){
+		var $thisCollapsible = $(this);
+		var $closer = $('<span>', {
+			class: "collapsible_closer",
+			click: function(){
+				$thisCollapsible.toggleClass('open');
+			}
+		});
+		var $header = $('<span>', {
+			class: "collapsible_header",
+			text: $thisCollapsible.data('header')
+		});
+		$thisCollapsible.prepend($closer);
+		$thisCollapsible.prepend($header);
 	});
-}
-
-function assembleAndDownloadFile(){
-	$('[data-save]').each(function(){
-		saveThisData($(this));
-	});
-
-	var modifiedTemplate = loadedFiles['template.html'].repeat(1);
-	$('[data-borksy-replace-single]').each(function(){
-		var $this = $(this);
-		var valueToReplace = 'BORKSY-' + $this.data('borksy-replace-single');
-		var formValue = $this.val();
-		modifiedTemplate = modifiedTemplate.replace(valueToReplace, formValue);
-	});
-	$('[data-borksy-replace-single]').promise().done(function(){
-		download('myBORKSYgame.html', modifiedTemplate);
-	});
-}
-
-function download(filename, text) {
-    var element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-    element.setAttribute('download', filename);
-
-    element.style.display = 'none';
-    document.body.appendChild(element);
-
-    element.click();
-
-    document.body.removeChild(element);
-    console.log("File '" + filename + "' downloaded")
-}
-
-function saveThisData($this){
-	var name = $this.attr('name');
-	var value = $this.val();
-	localStorage.setItem(name, value);
-	console.log("Key: '" + name + "' saved to localStorage");
-}
-
-function loadThisData($this){
-	var name = $this.attr('name');
-	var value = localStorage.getItem(name);
-	$this.val(value);
-	console.log(" Got key: " + name + " from localStorage");
-}
-
-function togglePartyMode(){
-	$('body').toggleClass('party');
 }
 			
 $(document).ready(function(){
+	activateCollapsibles();
 	loadTemplate();
 	loadDefaults();
 	$('#download-button').click(assembleAndDownloadFile);
