8fc8dd173b5c24d04ef08e56defc8f7e8ea1a704
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2018-06-30 21:17:26 -0400

    update persist
    - add old checkbox support
    - add old logging
    - add old regex cleaning support

--- src/scripts/persist.js
+++ src/scripts/persist.js
@@ -1,16 +1,41 @@
 var inputs = {};
 
+function cleanUsingRegEx($this, regExStr) {
+	var regex = new RegExp(regExStr, "g");
+	$this.val($this.val().replace(regex, ""));
+};
+
+function shortenString(value, length) {
+	length = length || 10;
+	var string = String(value);
+	var ending = string.length > length ? "..." : "";
+	return string.substring(0, length) + ending;
+}
+
 export default function persist($input, defaultValue) {
 	var name = $input.attr('id');
+	if (inputs[name]) {
+		throw new Error('A persistent input with the id "' + name + '" already exists');
+	}
+
+	var isCheckbox = $input.prop('type') === 'checkbox';
 	var savedValue = localStorage.getItem(name) || defaultValue;
-	$input.val(savedValue);
+	console.log(" Got key: " + name + " from localStorage: " + shortenString(savedValue));
+	isCheckbox ? $input.prop('checked', savedValue === "true") : $input.val(savedValue);
 	$input.on('change', function () {
-		localStorage.setItem(name, $input.val());
+		if ($input.data('clean-regex')) {
+			cleanUsingRegEx($input, $input.data('clean-regex'));
+		}
+		var value = String(isCheckbox ? $input.prop('checked') : $input.val());
+		localStorage.setItem(name, value);
+		console.log("Key: '" + name + "' saved to localStorage: " + shortenString(value));
 	});
+
 	inputs[name] = {
 		$input,
 		name,
-		defaultValue
+		defaultValue,
+		isCheckbox
 	};
 }
 
