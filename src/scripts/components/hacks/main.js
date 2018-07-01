69b3940cd732f17baf8b490f90481ed65a5eee9d
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2018-07-01 18:23:14 -0400

    refactor hack component
    missing conflicts + assembling correctly again

--- src/scripts/components/hacks/main.js
+++ src/scripts/components/hacks/main.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 import {
-	hacks, borksyInfo
+	hacks,
+	borksyInfo
 } from '../../libs';
 import {
 	makeNewCollapsible
@@ -8,63 +9,27 @@ import {
 import persist from '../../persist';
 
 import html from './.html';
+import loadHack from './loadHack';
+import parseHack from './parseHack';
 
-var loadedFiles = {};
-
-function loadFileFromPath(filename, pathToDir, doneCallback, failCallBack, filenameOverride) {
-	doneCallback = doneCallback || function () {};
-	failCallBack = failCallBack || function () {};
-	var $ajax = $.ajax(pathToDir + filename);
-	$ajax.done(function () {
-		filename = filenameOverride || filename;
-		loadedFiles[filename] = escape($ajax.responseText);
-		console.log('Loaded ' + filename + ' via AJAX');
-		doneCallback($ajax.responseText, filenameOverride);
-	});
-	$ajax.fail(function () {
-		//loadedFiles[filename] = "";
-		console.log('Error loading ' + filename + ' via AJAX');
-		failCallBack($ajax.responseText, filenameOverride);
-	});
-}
-
-function dashesToCamelCase(string) {
-	return string.toLowerCase().replace(/-(.)/g, function (match, group1) {
+function toCamelCase(string) {
+	return string.toLowerCase().replace(/(?:-|\s)(.)/g, function (match, group1) {
 		return group1.toUpperCase();
 	});
 }
 
-function removeExtraChars(string) {
-	return string.replace(/[^\w\s]/gi, '');
+function toTitleCase(string) {
+	return string
+		.replace(/-/g, ' ')
+		.split(' ')
+		.map(function (str) {
+			return str.substr(0, 1).toUpperCase() + str.substr(1);
+		})
+		.join(' ');
 }
 
-function arrayToSentenceFrag(arr) {
-	if (arr.length > 1) {
-		return arr.slice(0, arr.length - 1).join(', ') + ", and " + arr.slice(-1);
-	} else {
-		return arr[0];
-	}
-}
-
-function removeConflictingHacks(conflictsArr) {
-	$.each(conflictsArr, function (index, hackName) {
-		var $conflictingHack = $('#' + hackName);
-		var hiddenAndNotIncluded = $conflictingHack.prop('type') === 'hidden' && $conflictingHack.val() === false;
-		var checkboxAndNotIncluded = $conflictingHack.prop('type') === 'checkbox' && $conflictingHack.prop('checked') === false;
-		if (hiddenAndNotIncluded || checkboxAndNotIncluded) {
-			return;
-		}
-		$conflictingHack.val(false);
-		$conflictingHack.prop('checked', false);
-		saveThisHack($conflictingHack, false);
-	});
-}
-
-function checkAndToggleIncludedDisplay($thisField) {
-	var $collapsible = $('[data-associated-hack=' + $thisField.data('hack') + ']');
-	if ($collapsible.length > 0) {
-		toggleIncludedDisplay($collapsible, $thisField);
-	}
+function toSentenceCase(string) {
+	return (string.substr(0, 1).toUpperCase() + string.substr(1)).replace(/\.?$/, '.');
 }
 
 function toggleIncludedDisplay($collapsible, $thisHack) {
@@ -73,24 +38,6 @@ function toggleIncludedDisplay($collapsible, $thisHack) {
 	} else {
 		$collapsible.removeClass('included');
 	}
-}
-
-function saveThisHack($thisHack, checkConflicts) {
-	if (typeof (checkConflicts) === 'undefined') {
-		checkConflicts = true;
-	}
-	checkAndToggleIncludedDisplay($thisHack);
-
-	var thisConflicts = hacks[$thisHack.data('hack')].conflicts;
-	if (thisConflicts && checkConflicts) {
-		removeConflictingHacks(thisConflicts.split(','));
-	}
-}
-
-function hackIncludeTrigger($this) {
-	$this.change(function () {
-		saveThisHack($this);
-	});
 }
 
 function reOrderHacks() {
@@ -130,215 +77,123 @@ export function assembleHacks() {
 	return hackBundle;
 }
 
-function localHackSuccess(response, filename) {
-	var hackName = filename.substr(0, filename.lastIndexOf('.')) || filename;
-	//var hackName = filename.substring(0,filename.length - 3);
-	$("#hacks-section").append(createThisHackMenu(hackName, hacks[hackName]));
-}
+function createThisHackMenu(options) {
+	var title = toTitleCase(options.file);
+	var $collapse = makeNewCollapsible(options.emoji + ' ' + title + ' (By ' + options.author + ')');
+	$collapse.attr('data-associated-hack', options.file);
 
-function localHackFail(response, filename) {
-	console.error(response, filename);
-}
-
-function loadThisHackLocally(hackName) {
-	hackName = hackName.substr(0, hackName.lastIndexOf('.')) || hackName;
-	var filename = hackName + ".js"
-	var pathToDir = "hacks/dist/";
-	loadFileFromPath(filename, pathToDir, localHackSuccess, localHackFail, filename)
-}
-
-function githubHackSuccess(response, filename) {
-	var hackName = filename.substr(0, filename.lastIndexOf('.')) || filename;
-	hacks[hackName].usingGithub = true;
-	$("#hacks-section").append(createThisHackMenu(hackName, hacks[hackName]));
-}
-
-function githubHackFail(response, filename) {
-	var hackName = filename.substr(0, filename.lastIndexOf('.')) || filename;
-	hacks[hackName].usingGithub = false;
-	loadThisHackLocally(filename, hacks[hackName]);
-}
-
-function loadThisHackFromGithub(hackName, hackInfo) {
-	var filenameOverride = hackName + '.js';
-	var filename = hackInfo.github;
-	var pathToDir = "https://raw.githubusercontent.com/seleb/bitsy-hacks/master/dist/";
-	loadFileFromPath(filename, pathToDir, githubHackSuccess, githubHackFail, filenameOverride);
-}
-
-function bakeHackData($element, hackName, hackInfo) {
-	$element.attr({
-		'data-save': true,
-		'data-default': false,
-		'data-default-type': "boolean",
-		'data-hack': hackName,
-		'data-hack-type': hackInfo.type
+	var $summary = $('<p>', {
+		text: toSentenceCase(options.summary)
 	});
-	if (hackInfo.requires) {
-		$element.attr('data-requires', hackInfo.requires);
-	}
-}
+	$collapse.append($summary);
 
-function hackMenuConflicts(hackName, hackInfo, $parentCollapse) {
-	var conflictTitlesArr = []
-	$.each(hackInfo.conflicts.split(','), function (index, conflictName) {
-		conflictTitlesArr.push(removeExtraChars(hacks[conflictName].title));
-	});
-	var sentenceFrag = arrayToSentenceFrag(conflictTitlesArr);
-	var $warning = $('<p>', {
-		text: 'This hack conflicts with ' + sentenceFrag + '.',
-		class: 'conflict-warning'
-	});
-	$parentCollapse.append($warning);
-}
+	// TODO: conflicts
 
-function hackGitHubMessage(hackName, hackInfo, $parentCollapse) {
-	var className = "github-message";
-	var msg = "";
-	var hackTitle = removeExtraChars(hackInfo.title);
-	if (hackInfo.forceLocal !== false) {
-		msg = 'Borksy is opting to use a local version of ' + hackTitle + ' from ' + hackInfo.forceLocal + '.';
-	} else if (hacks[hackName].usingGithub === true) {
-		msg = hackTitle + ' is using the most recent version from Github.';
+	// from github/local message
+	var $sourceMessage = $('<p class="github-message">');
+	if (options.forceLocal) {
+		$sourceMessage.text('Borksy is opting to use a local version of ' + title + '.');
+	} else if (!options.isLocal) {
+		$sourceMessage.text(title + ' is using the most recent version from Github.');
 	} else {
-		msg = hackTitle + ' could not be loaded from Github, local version retrieved on ' + borksyInfo.lastUpdated + ' is being used.';
-		className += " warning";
+		$sourceMessage.text(title + ' could not be loaded from Github, local version retrieved on ' + borksyInfo.lastUpdated + ' is being used.');
+		$sourceMessage.addClass('warning');
 	}
-	var $message = $('<p>', {
-		text: msg,
-		class: className
-	});
-	$parentCollapse.append($message);
-}
+	$collapse.append($sourceMessage);
 
-function hackMenuPython(hackName, hackInfo, $parentCollapse) {
-	var $friendliness = makeNewCollapsible(removeExtraChars(hackInfo.title) + " Bitspy Friendliness:");
-	var sentenceStart = "Use of this Hack ";
-	var sentenceEnd = "";
-	var bitspyLink = "<a href='https://github.com/Ragzouken/bitspy'>the Python Player for Bitsy</a> ";
-	bitspyLink += "(IE: <a href='https://candle.itch.io/bitsy-boutique'>The Bitsy Boutique</a>)";
-	var fine = "The features in this hack will not be available, but your game should otherwise function normally."
-	var notfine = "Your game will likely appear broken."
-	switch (hackInfo.python) {
-		case "green":
-			sentenceStart += "does not interfere with ";
-			sentenceEnd = ". " + fine;
-			break;
-		case "yellow":
-			sentenceStart += "is possible with ";
-			sentenceEnd = " if and only if there is no script reference to it in dialog. " + fine;
-			break;
-		case "red":
-		default:
-			sentenceStart += "is not compatible with ";
-			sentenceEnd += ". " + notfine;
-			break;
-	}
-	var $text = $('<p>', {
-		html: sentenceStart + bitspyLink + sentenceEnd
-	});
-	$friendliness.addClass('python').addClass(hackInfo.python).append($text);
-	$parentCollapse.append($friendliness);
-}
-
-function hackMenuOptions(hackName, hackInfo, $parentCollapse) {
-	var $options = makeNewCollapsible(removeExtraChars(hackInfo.title) + " Options:");
-	var $optionsLabel = $('<label>', {
-		text: "var " + dashesToCamelCase(hackName) + "Options = {"
-	});
-	loadFileFromPath(hackName + '.options.txt', 'hacks/options/', function (responseText) {
-		var $optionsField = $('<textarea>', {
-			rows: 5,
-			cols: 50,
-			text: responseText,
-			name: hackName + '.options',
-			id: hackName + '-options'
-		});
-		$optionsField.attr({
-			'data-save': true,
-			'data-default-type': "hackOptions",
-			'data-default': hackName + '.options.txt'
-		});
-		persist($optionsField);
-		$optionsLabel.append($optionsField);
-		$optionsLabel.append(document.createTextNode("};"));
-		$options.append($optionsLabel);
-		$parentCollapse.append($options);
-	});
-}
-
-function hackMenuReadme(hackName, hackInfo, $parentCollapse) {
-	var $readme = makeNewCollapsible(removeExtraChars(hackInfo.title) + " README:");
-	loadFileFromPath(hackName + '.readme.txt', 'hacks/info/', function (responseText) {
-		var $pre = $('<pre>', {
-			text: responseText
-		});
-		$readme.append($pre);
-		$parentCollapse.append($readme);
-	});
-}
-
-function createThisHackMenu(hackName, hackInfo) {
-	var $collapse = makeNewCollapsible(hackInfo.title + " (By " + hackInfo.author + ")");
-	$collapse.attr('data-associated-hack', hackName);
-
-	var $description = $('<p>', {
-		text: hackInfo.description
-	});
-	$collapse.append($description);
-
-	if (hackInfo.conflicts) {
-		hackMenuConflicts(hackName, hackInfo, $collapse);
-	}
-
-	hackGitHubMessage(hackName, hackInfo, $collapse);
-
+	// checkbox
 	var $label = $('<label>', {
-		text: "Include " + removeExtraChars(hackInfo.title)
+		text: "Include " + title
 	});
 	var $checkbox = $('<input>', {
 		type: 'checkbox',
-		name: hackName,
-		id: hackName
+		name: options.file,
+		id: options.file,
 	});
-	bakeHackData($checkbox, hackName, hackInfo);
-	persist($checkbox);
+	persist($checkbox, false);
 	toggleIncludedDisplay($collapse, $checkbox);
-	hackIncludeTrigger($checkbox);
+	$checkbox.change(function () {
+		toggleIncludedDisplay($collapse, $checkbox);
+	});
 	$label.append($checkbox);
 	$collapse.append($label);
 
-	if (hackInfo.type === "options") {
-		hackMenuOptions(hackName, hackInfo, $collapse);
+	// bitspy friendliness
+	var $friendliness = makeNewCollapsible(title + " Bitspy Friendliness:");
+	var fine = 'The features in this hack will not be available, but your game should otherwise function normally.';
+	var sentence = 'Use of this Hack ' +
+		({
+			green: 'does not interfere with ',
+			yellow: 'is possible with ',
+			red: 'is not compatible with '
+		}[options.python]) +
+		"<a href='https://github.com/Ragzouken/bitspy'>the Python Player for Bitsy</a> (IE: <a href='https://candle.itch.io/bitsy-boutique'>The Bitsy Boutique</a>)" +
+		({
+			green: '. ' + fine,
+			yellow: ' if and only if there is no script reference to it in dialog. ' + fine,
+			red: '. Your game will likely appear broken.'
+		}[options.python]);
+	var $text = $('<p>', {
+		html: sentence
+	});
+	$friendliness.addClass('python').addClass(options.python).append($text);
+	$collapse.append($friendliness);
+
+	// options
+	if (options.options) {
+		var $options = makeNewCollapsible(title + " Options:");
+		var $optionsLabel = $('<label>', {
+			text: "var " + toCamelCase(options.file) + "Options = {"
+		});
+		var $optionsField = $('<textarea>', {
+			rows: 5,
+			text: options.options,
+			name: options.file + '.options',
+			id: options.file + '-options'
+		});
+		$optionsField.prop('wrap', 'off');
+		persist($optionsField, options.options);
+		$optionsLabel.append($optionsField);
+		$optionsLabel.append(document.createTextNode("};"));
+		$options.append($optionsLabel);
+		$collapse.append($options);
 	}
 
-	if (hackInfo.readme === true) {
-		hackMenuReadme(hackName, hackInfo, $collapse);
+	// readme
+	if (options.readme) {
+		var $readme = makeNewCollapsible(title + " README:");
+		var $pre = $('<pre>', {
+			text: options.description
+		});
+		$readme.append($pre);
+		$collapse.append($readme);
 	}
-
-	hackMenuPython(hackName, hackInfo, $collapse);
 
 	return $collapse;
 }
 
-function loadThisHack(hackName, hackInfo) {
-	if (hackInfo.forceLocal !== false) {
-		loadThisHackLocally(hackName, hackInfo)
-	} else if (hackInfo.github !== false) {
-		loadThisHackFromGithub(hackName, hackInfo)
-	} else {
-		// there's no dist version of kitsy/utils rn
-	}
-}
-
-function createHackMenus() {
-	$.each(hacks, function (hackName, hackObj) {
-		loadThisHack(hackName, hackObj);
-	});
-}
 
 var $html = $(html);
 
-createHackMenus();
+Promise.all(
+		Object.values(hacks)
+		.map(async function (hack) {
+			try {
+				var loaded = await loadHack(hack);
+				var hackInfo = parseHack(loaded.hackFile);
+				var $hackMenu = createThisHackMenu(Object.assign({}, loaded, hack, hackInfo));
+				return $hackMenu;
+			} catch (error) {
+				console.error(error);
+			}
+		}))
+	.then(function (hackMenus) {
+		hackMenus
+			.filter(function ($hackMenu) {
+				return $hackMenu;
+			}).forEach(function ($hackMenu) {
+				$html.append($hackMenu);
+			});
+	});
 
 export default $html;
