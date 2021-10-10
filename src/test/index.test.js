8027e1219a41e0355ccf991bcbffa7885367eb81
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-10-10 02:47:26 -0400

    test that hack is actually applied

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -41,14 +41,36 @@ describe('Borksy', () => {
 		expect(await page.screenshot()).toMatchImageSnapshot();
 	});
 
-	it('should allow game to be downloaded', async () => {
+	it('should allow game to be customized', async () => {
 		// fill out game data
 		await page.click('form .collapsible:first-of-type .collapsible_header'); // open title
 		await page.type('#title', 'Custom Title');
 		await page.type('#filename', 'Custom Filename');
+		expect(await page.screenshot()).toMatchImageSnapshot();
 
-		// TODO: enable some representative/testable hacks
+		// remove collision on walls
+		await page.click('form .collapsible:nth-of-type(3) .collapsible_header'); // open gamedata
+		await page.focus('#gamedata');
+		await page.keyboard.down('Control');
+		await page.keyboard.press('Home');
+		await page.keyboard.up('Control');
+			for (let i = 0; i < 39; ++i) {
+			await page.keyboard.press('ArrowDown');
+		}
+		await page.keyboard.down('Shift');
+		await page.keyboard.press('ArrowDown');
+		await page.keyboard.up('Shift');
+		await page.keyboard.press('Backspace');
+		expect(await page.screenshot()).toMatchImageSnapshot();
 
+		// enable transparent sprites hack
+		await page.click('#hacks-section > .collapsible_header'); // open hacks
+		await page.click('.collapsible[data-associated-hack="transparent-sprites"] > .collapsible_header'); // open transparent sprites
+		await page.click('.collapsible[data-associated-hack="transparent-sprites"] input[type="checkbox"]'); // click checkbox
+		expect(await page.screenshot()).toMatchImageSnapshot();
+	});
+
+	it('should allow game to be downloaded', async () => {
 		// remove download if we already have one
 		if (fs.existsSync(download)) {
 			await fs.promises.unlink(download);
@@ -70,8 +92,25 @@ describe('Borksy', () => {
 		expect(await page.screenshot()).toMatchImageSnapshot();
 		await page.keyboard.down('ArrowRight'); // end title dialog
 		await page.waitForTimeout(100);
-		await page.keyboard.up('ArrowRight'); // complete title dialog
+		await page.keyboard.up('ArrowRight'); // end title dialog
 		await page.waitForTimeout(100);
 		expect(await page.screenshot()).toMatchImageSnapshot();
-	})
+	});
+
+	it('should produce a game with hacks applied', async () => {
+		// walk on top of wall to demonstrate transparent sprites
+		await page.keyboard.down('ArrowLeft');
+		await page.waitForTimeout(100);
+		await page.keyboard.up('ArrowLeft');
+		await page.waitForTimeout(100);
+		await page.keyboard.down('ArrowLeft');
+		await page.waitForTimeout(100);
+		await page.keyboard.up('ArrowLeft');
+		await page.waitForTimeout(100);
+		await page.keyboard.down('ArrowLeft');
+		await page.waitForTimeout(100);
+		await page.keyboard.up('ArrowLeft');
+		await page.waitForTimeout(100);
+		expect(await page.screenshot()).toMatchImageSnapshot();
+	});
 });
