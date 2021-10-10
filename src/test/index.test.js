8f6a127415e2865771f47194a999e23946099c16
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-10-10 01:58:36 -0400

    test that game can be downloaded + played

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -15,6 +15,7 @@ describe('Borksy', () => {
 		});
 		page = await browser.newPage();
 
+		await page._client.send('Page.setDownloadBehavior', { behavior: 'allow', downloadPath: path.resolve(__dirname) });
 		await page.goto(`file:///${path.resolve(__dirname, '../../docs/index.html').replace(/\\/g, '/')}`, {
 			waitUntil: 'networkidle2',
 		});
@@ -32,4 +33,27 @@ describe('Borksy', () => {
 	it('should load', async () => {
 		expect(await page.screenshot()).toMatchImageSnapshot();
 	});
+	it('should allow game to be downloaded', async () => {
+		await page.click('form .collapsible:first-of-type .collapsible_header'); // open title
+		await page.type('#title', 'Custom Title');
+		await page.type('#filename', 'Custom Filename');
+		await page.click('#download-button');
+		await page.waitForTimeout(2000); // TODO: replace with actually checking for file
+	});
+
+	it('should produce a playable game', async () => {
+		await page.goto(`file:///${path.resolve(__dirname, 'myBORKSYgameCustomFilename.html').replace(/\\/g, '/')}`, {
+			waitUntil: 'networkidle2',
+		});
+		await page.keyboard.down('ArrowRight'); // complete title dialog
+		await page.waitForTimeout(100);
+		await page.keyboard.up('ArrowRight'); // complete title dialog
+		await page.waitForTimeout(100);
+		expect(await page.screenshot()).toMatchImageSnapshot();
+		await page.keyboard.down('ArrowRight'); // end title dialog
+		await page.waitForTimeout(100);
+		await page.keyboard.up('ArrowRight'); // complete title dialog
+		await page.waitForTimeout(100);
+		expect(await page.screenshot()).toMatchImageSnapshot();
+	})
 });
