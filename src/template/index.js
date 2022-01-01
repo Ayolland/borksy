dccf1fb44a30ee79cb3dc22471ee94ddf1e451f5
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2022-01-01 16:48:19 -0500

    feat: added Bitsy 7.11 template

--- src/template/index.js
+++ src/template/index.js
@@ -5,6 +5,7 @@ import Bitsy64 from './Bitsy.6.4.txt';
 import Bitsy65 from './Bitsy.6.5.txt';
 import Bitsy70 from './Bitsy.7.0.txt';
 import Bitsy710 from './Bitsy.7.10.txt';
+import Bitsy711 from './Bitsy.7.11.txt';
 import Bitsy72 from './Bitsy.7.2.txt';
 import Bitsy78 from './Bitsy.7.8.txt';
 import BitsyHD51 from './BitsyHD.5.1.txt';
@@ -19,5 +20,6 @@ export default [
 	{ data: Bitsy70, id: 'Bitsy70', description: 'Bitsy 7.0' },
 	{ data: Bitsy72, id: 'Bitsy72', description: 'Bitsy 7.2' },
 	{ data: Bitsy78, id: 'Bitsy78', description: 'Bitsy 7.8' },
-	{ data: Bitsy710, id: 'Bitsy710', description: 'Bitsy 7.10', isDefault: true },
+	{ data: Bitsy710, id: 'Bitsy710', description: 'Bitsy 7.10' },
+	{ data: Bitsy711, id: 'Bitsy711', description: 'Bitsy 7.11', isDefault: true },
 ];
