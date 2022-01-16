2095dea7309fe3b51768b2ca5f026fe7eb521c21
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2022-01-16 14:26:00 -0500

    chore: add rollup bundle visualizer

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
+import visualizer from 'rollup-plugin-visualizer';
 import mdPlugin, { Mode } from 'vite-plugin-markdown';
 import strPlugin from 'vite-plugin-string';
 
-/** @type {import('vite').UserConfig} */
-export default {
+/** @type {() => import('vite').UserConfig} */
+export default ({ command }) => ({
 	root: 'src',
 	base: './',
 	build: {
 		outDir: '../docs',
 		emptyOutDir: true,
+		rollupOptions: command === 'build' && {
+			plugins: [visualizer({ open: true })],
+		},
 	},
 	plugins: [
 		strPlugin({
@@ -18,4 +22,4 @@ export default {
 			mode: Mode.HTML,
 		}),
 	],
-};
+});
