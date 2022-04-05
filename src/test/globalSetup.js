451134a9c714cad6e302a618ebb7b5d12a4cf49a
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2022-04-04 21:36:56 -0400

    test: fix test server setup

--- src/test/globalSetup.js
+++ src/test/globalSetup.js
@@ -2,7 +2,7 @@ const { setup } = require('jest-dev-server');
 
 module.exports = async () => {
 	await setup({
-		command: 'npm run serve',
+		command: 'npm run serve -- --port 5000',
 		protocol: 'http',
 		port: 5000,
 	});
