b3cb6d7c37923e9b252e61a2af95d7129fe73510
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2023-06-01 16:38:03 -0400

    add `host`

--- src/test/globalSetup.js
+++ src/test/globalSetup.js
@@ -3,6 +3,7 @@ const { setup } = require('jest-dev-server');
 module.exports = async () => {
 	await setup({
 		command: 'npm run serve -- --port 5000',
+		host: 'localhost',
 		protocol: 'http',
 		port: 5000,
 		launchTimeout: 10000,
